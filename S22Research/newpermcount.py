4f7edefc55acee0428cf7906bf6cce232545a6fe
austinbeal14 <121691886+austinbeal14@users.noreply.github.com>
2023-10-17 14:00:58 -0400

    Added S22 Python Files
    These are test files that were used to work out various cases of the Circulant TSP. These scripts were automated to a HPC system, and most of them produce text files with results. Explanantions of these results will be added in the future

---
+++ S22Research/newpermcount.py
@@ -0,0 +1,42 @@
+from itertools import *
+from sortedcontainers import SortedList
+import math
+import sys
+def main():
+    n =int(sys.argv[1])
+    num = math.floor(n/2)
+    s = ""
+    for i in range(2, n+1):
+        s += str(i)
+    #unique_edges = []
+    unique_edges = SortedList([])
+    perms = list(permutations(s))
+    for perm in perms:
+        t = (1,)
+        perm = t + perm
+        dist_vector = []
+        dist_vector = get_edge_vector(perm, n)
+        if dist_vector not in unique_edges:
+            unique_edges.add(dist_vector)
+                #unique_edges.append(dist_vector)
+    print(unique_edges)
+    print("Count: " + str(len(unique_edges)))
+
+def get_edge_vector(l, n):
+    edge_list = [0]*math.floor(n/2)
+    for i in range(len(l) - 1):
+        n1 = int(l[i])
+        n2 = int(l[i+1])
+        dist = abs(n1 - n2)
+        if dist > math.floor(n/2):
+            dist = n - dist
+        edge_list[dist-1] += 1
+    n1 = int(l[0])
+    n2 = int(l[len(l) - 1])
+    dist = abs(n1 - n2)
+    if dist > math.floor(n/2):
+        dist = n - dist
+    edge_list[dist -1] += 1
+    return edge_list
+main()
+
